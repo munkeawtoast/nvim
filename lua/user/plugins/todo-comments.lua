2e43f1f4b932f1a5c3ce1c65a2bd1469861faa81
munkeawtoast <munkeawtoast@duck.com>
2025-07-26 19:36:42 +0700

    feat: add dadbod

--- lua/user/plugins/todo-comments.lua
+++ lua/user/plugins/todo-comments.lua
@@ -1,19 +1,4 @@
--- return {
--- 	"folke/todo-comments.nvim",
--- 	enabled = false,
--- 	dependencies = { "nvim-lua/plenary.nvim" },
--- 	opts = {
--- 		-- FIXME: this is a test
--- 		signs = false,
--- 		highlight = {
--- 			multiline = false,
--- 			keyword = "fg",
--- 			after = "",
--- 		},
--- 		gui_style = {
--- 			fg = "NONE", -- The gui style to use for the fg highlight group.
--- 			bg = "NONE", -- The gui style to use for the bg highlight group.
--- 		},
--- 	},
--- }
-return {}
+return {
+	"folke/todo-comments.nvim",
+	dependencies = { "nvim-lua/plenary.nvim" },
+}
